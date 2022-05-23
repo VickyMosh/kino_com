888f0055e96c87fe9759cebd3c88827ccdd6c767
VickyMosh <victoria_mosh@mail.ru>
2022-05-24 02:05:49 +0300

    var9

--- lab_ff/kino_comment/comments/urls.py
+++ lab_ff/kino_comment/comments/urls.py
@@ -4,8 +4,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name='home'),   #при переходе на гл стр обращаемся к методу индекс в вью
-    path('movies', views.movie, name='movie'),
-    path('cartoons', views.cartoon, name='cartoon'),
-    path('outlog', views.outlog, name='outlog'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+                  path('', views.index, name='home'),  # при переходе на гл стр обращаемся к методу индекс в вью
+                  path('movies', views.movie, name='movie'),
+                  path('cartoons', views.cartoon, name='cartoon'),
+                  path('outlog', views.outlog, name='outlog'),
+                  path('leon_full', views.leon_full, name='leon_full'),
+                  path('koko_full', views.koko_full, name='koko_full'),
+                  path('soul_full', views.soul_full, name='soul_full'),
+                  path('pirates_full', views.pirates_full, name='pirates_full'),
+                  path('dolmatines_full', views.dolmatines_full, name='dolmatines_full'),
+                  path('wishes_full', views.wishes_full, name='wishes_full')
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- lab_ff/kino_comment/comments/views.py
+++ lab_ff/kino_comment/comments/views.py
@@ -12,3 +12,21 @@ def cartoon(request):
 
 def outlog(request):
     return render(request, 'registration/outlog.html')
+
+def leon_full(request):
+    return render(request, 'comments/leon_full.html')
+
+def koko_full(request):
+    return render(request, 'comments/koko_full.html')
+
+def soul_full(request):
+    return render(request, 'comments/soul_full.html')
+
+def pirates_full(request):
+    return render(request, 'comments/pirates_full.html')
+
+def dolmatines_full(request):
+    return render(request, 'comments/dolmatines_full.html')
+
+def wishes_full(request):
+    return render(request, 'comments/wishes_full.html')
